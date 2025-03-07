24ab1cbfbb43864b94afe050c0999f7289607aea
Abir Shahadat Purab <a.s.purab0@gmail.com>
2025-03-07 21:53:35 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'tan': '#B68C5A',
+          'dark-tan': '#C9883A',
+        },
+        borderRadius: {
+          'custom': '12px',
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  };
+  
